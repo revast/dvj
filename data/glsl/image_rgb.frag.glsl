92f1f84997d4d17881f133d6293503ab67b4efe3
interim.descriptor <interim.descriptor@0f8f9bc0-b742-0410-b8d9-111abb37ccfb>
2011-06-10 04:17:21 +0000

    Update shader

--- data/glsl/image_rgb.frag.glsl
+++ data/glsl/image_rgb.frag.glsl
@@ -1,12 +1,19 @@
 //image_rgb.frag.glsl
 
-uniform sampler2D tex;
+uniform sampler2D texLeft;
+uniform sampler2D texCenter;
+uniform sampler2D texRight;
 uniform float brightnessScalar;
+uniform float rgbSpatializerScalar;
 
 void main()
 {
-	vec4 texColor = texture2D(tex,gl_TexCoord[0].st);
-	gl_FragColor = gl_Color * texColor * brightnessScalar;
+	vec4 texColorLeft = texture2D(texLeft,gl_TexCoord[0].st + vec2(-0.01*rgbSpatializerScalar,0));
+	vec4 texColorCenter = texture2D(texCenter,gl_TexCoord[0].st);
+	vec4 texColorRight = texture2D(texRight,gl_TexCoord[0].st + vec2(0.01*rgbSpatializerScalar,0));
+	gl_FragColor.r = gl_Color.r * texColorLeft.r * brightnessScalar;
+	gl_FragColor.g = gl_Color.g * texColorCenter.g * brightnessScalar;
+	gl_FragColor.b = gl_Color.b * texColorRight.b * brightnessScalar;
 	gl_FragColor.a = gl_Color.a;
 }
 
