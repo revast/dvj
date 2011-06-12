8d668342e58bedb425c5179d6bb2ecca4d5d2fa4
interim.descriptor <interim.descriptor@0f8f9bc0-b742-0410-b8d9-111abb37ccfb>
2011-06-12 02:09:38 +0000

    Clean all dependencies

---
+++ trunk/dep/src/clean.sh
@@ -0,0 +1,42 @@
+cd jpeg-6b
+make clean
+cd ..
+
+cd libpng-1.2.37
+make clean
+cd ..
+
+cd SDL
+make clean
+cd ..
+
+cd SDL_image-1.2.7
+make clean
+cd ..
+
+cd SDL_net-1.2.7
+make clean
+cd ..
+
+cd fftw-3.2.1
+make clean
+cd ..
+
+cd libsamplerate-0.1.7
+make clean
+cd ..
+
+cd yasm-1.0.1
+make clean
+cd ..
+
+cd ffmpeg
+make clean
+cd ..
+
+cd jack-audio-connection-kit-0.118.0
+sudo rm /usr/local/lib/libjack*
+sudo rm -rf /usr/local/lib/jack
+make clean
+cd ..
+
