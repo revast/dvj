e3d5b9935769016306efa6da667925129533afac
interim.descriptor <interim.descriptor@0f8f9bc0-b742-0410-b8d9-111abb37ccfb>
2012-05-27 21:42:16 +0000

    Stub out call to hg

--- dep/src/SDL/build-scripts/showrev.sh
+++ dep/src/SDL/build-scripts/showrev.sh
@@ -4,4 +4,6 @@
 
 # FIXME: this prints the tip, which isn't useful if you're on a different
 #  branch, or just not sync'd to the tip.
-hg tip --template 'hg-{rev}:{node|short}' || echo "hg-0:baadf00d"
+#hg tip --template 'hg-{rev}:{node|short}' || echo "hg-0:baadf00d"
+echo "hg-0:baadf00d"
+
