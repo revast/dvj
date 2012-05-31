bc0945e2f539cdd5e8a5de74d06bdb12caf7257d
interim.descriptor <interim.descriptor@0f8f9bc0-b742-0410-b8d9-111abb37ccfb>
2012-05-31 05:31:33 +0000

    Hello World works.

---
+++ trunk/tools/helloWorld/src/Main.cpp
@@ -0,0 +1,29 @@
+/*
+ *
+ * Main.cpp
+ *
+ */
+
+#include "LGL.module/LGL.h"
+
+int
+main
+(
+	int	argc,
+	char**	argv
+)
+{
+	LGL_Init
+	(
+		640,480,
+		2,
+		"testFrameRate"
+	);
+
+	for(;;)
+	{
+		LGL_DrawFPSGraph();
+		LGL_SwapBuffers();
+	}
+}
+
